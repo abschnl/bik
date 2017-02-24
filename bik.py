a55760cc3f34c5f4dbbb06b2e159a11377383a5c
祖丰泽 <zfz@duiba.com.cn>
2017-02-24 18:42:40 +0800

    增加登陆及翻页

--- bik.py
+++ bik.py
@@ -11,22 +11,72 @@ import http.cookiejar
 import time
 import os
 
+TOKEN = ""
 
-def dowloadImg(imgUrl, imgName):
+def dowloadImg(imgUrl, imgName, title):
 
     url = 'https://storage1.picacomic.com/static/'+imgUrl
 
     try:
-        response = urllib.request.urlopen(url,timeout=3)
+        response = urllib.request.urlopen(url)
     except Exception as e:
         print("timeOut : "+url)
 
     data = response.read()
 
-    with open("./img/"+imgName, "wb") as code:
+    if os.path.exists("./img/"+title+"/"):
+        pass
+    else:
+        os.mkdir("./img/"+title+"/")
+
+    with open("./img/"+title+"/"+imgName, "wb") as code:
         code.write(data)
 
+def login():
+    url = 'https://picaapi.picacomic.com/auth/sign-in'
+
+    values = {
+        'email': '',
+        'password': ''
+    }
+
+    user_agent = 'sora/2.2 (com.picacomic.sora; build:2.2; iOS 10.0.1) Alamofire/4.0.1'
+
+    headers = {
+        'User-Agent': user_agent,
+        'Host': 'picaapi.picacomic.com',
+        'Accept': 'application/vnd.picacomic.com.v1+json',
+        'app-version': '2.0.1.3',
+        'api-key': '2587EFC6F859B4E3A1D8B6D33B272',
+        'app-platform': 'ios',
+        #    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
+        #    'Accept-Language': 'zh-Hans-CN;q=1.0',
+        'app-uuid': '8E4C6215-FF1E-4661-986C-226B841B8138',
+        'Connection': 'keep-alive'
+    }
+
+    url_values = urllib.parse.urlencode(values)
+
+    url_values = url_values.encode(encoding='UTF8')
+
+    full_url = urllib.request.Request(url, url_values, headers)
+
+    response = urllib.request.urlopen(full_url)
+
+    the_page = response.read().decode("utf-8")
+
+    jsonStr = json.loads(the_page)
+
+    global TOKEN
+
+    TOKEN = jsonStr["data"]["token"]
+
+    getList()
+
+
 def getList():
+
+    global TOKEN
 
     url = 'http://picaapi.picacomic.com/comics'
 
@@ -41,15 +91,14 @@ def getList():
         'User-Agent': user_agent,
         'Host': 'picaapi.picacomic.com',
         'Accept': 'application/vnd.picacomic.com.v1+json',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFic2NobmxAZ21haWwuY29tIiwicm9sZSI6Im1lbWJlciIsIm5hbWUiOiJhYnNjaG5sIiwidmVyc2lvbiI6IjIuMC4xLjMiLCJpYXQiOjE0ODcxNjQyNzIsImV4cCI6MTQ4Nzc2OTA3Mn0.2qPBopttGOdBdsSgdHYjh3Jpi4cQJ1nOvZFfukfw2BY',
+        'Authorization': TOKEN,
         'app-version': '2.0.1.3',
         'api-key': '2587EFC6F859B4E3A1D8B6D33B272',
         'app-platform': 'ios',
         #    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
         #    'Accept-Language': 'zh-Hans-CN;q=1.0',
         'app-uuid': '8E4C6215-FF1E-4661-986C-226B841B8138',
-        'Connection': 'keep-alive',
-        'Cookie': '__cfduid=ddcc480f60c67d57be2b9b9ddad3ee5a01487654209'
+        'Connection': 'keep-alive'
     }
 
     url_values = urllib.parse.urlencode(values)
@@ -60,13 +109,21 @@ def getList():
 
     response = urllib.request.urlopen(full_url)
 
-    the_page = response.read()
+    the_page = response.read().decode("utf-8")
 
-    print(the_page.decode("UTF8"))
+    jsonStr = json.loads(the_page)
 
-def getCover():
+    for e in (jsonStr["data"]["comics"]["docs"]):
+        print(e["_id"])
 
-    url = 'https://picaapi.picacomic.com/comics/58ac020c53ab5d740afce911'
+        getCover(e["_id"])
+
+
+def getCover(id):
+
+    global TOKEN
+
+    url = 'https://picaapi.picacomic.com/comics/'+id
 
     user_agent = 'sora/2.2 (com.picacomic.sora; build:2.2; iOS 10.0.1) Alamofire/4.0.1'
 
@@ -74,47 +131,17 @@ def getCover():
         'User-Agent': user_agent,
         'Host': 'picaapi.picacomic.com',
         'Accept': 'application/vnd.picacomic.com.v1+json',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFic2NobmxAZ21haWwuY29tIiwicm9sZSI6Im1lbWJlciIsIm5hbWUiOiJhYnNjaG5sIiwidmVyc2lvbiI6IjIuMC4xLjMiLCJpYXQiOjE0ODcxNjQyNzIsImV4cCI6MTQ4Nzc2OTA3Mn0.2qPBopttGOdBdsSgdHYjh3Jpi4cQJ1nOvZFfukfw2BY',
+        'Authorization':TOKEN,
         'app-version': '2.0.1.3',
         'api-key': '2587EFC6F859B4E3A1D8B6D33B272',
         'app-platform': 'ios',
         #    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
         #    'Accept-Language': 'zh-Hans-CN;q=1.0',
         'app-uuid': '8E4C6215-FF1E-4661-986C-226B841B8138',
-        'Connection': 'keep-alive',
-        'Cookie': '__cfduid=ddcc480f60c67d57be2b9b9ddad3ee5a01487654209'
+        'Connection': 'keep-alive'
     }
 
     full_url = urllib.request.Request(url, headers = headers)
-
-    response = urllib.request.urlopen(full_url)
-
-    the_page = response.read()
-
-    print(the_page.decode("UTF8"))
-
-def getImgs():
-
-    url = 'https://picaapi.picacomic.com/comics/58ac020c53ab5d740afce911/order/1/pages?page=1'
-
-    user_agent = 'sora/2.2 (com.picacomic.sora; build:2.2; iOS 10.0.1) Alamofire/4.0.1'
-
-    headers = {
-        'User-Agent': user_agent,
-        'Host': 'picaapi.picacomic.com',
-        'Accept': 'application/vnd.picacomic.com.v1+json',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFic2NobmxAZ21haWwuY29tIiwicm9sZSI6Im1lbWJlciIsIm5hbWUiOiJhYnNjaG5sIiwidmVyc2lvbiI6IjIuMC4xLjMiLCJpYXQiOjE0ODcxNjQyNzIsImV4cCI6MTQ4Nzc2OTA3Mn0.2qPBopttGOdBdsSgdHYjh3Jpi4cQJ1nOvZFfukfw2BY',
-        'app-version': '2.0.1.3',
-        'api-key': '2587EFC6F859B4E3A1D8B6D33B272',
-        'app-platform': 'ios',
-        #    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
-        #    'Accept-Language': 'zh-Hans-CN;q=1.0',
-        'app-uuid': '8E4C6215-FF1E-4661-986C-226B841B8138',
-        'Connection': 'keep-alive',
-        'Cookie': '__cfduid=ddcc480f60c67d57be2b9b9ddad3ee5a01487654209'
-    }
-
-    full_url = urllib.request.Request(url, headers=headers)
 
     response = urllib.request.urlopen(full_url)
 
@@ -122,9 +149,53 @@ def getImgs():
 
     jsonStr = json.loads(the_page)
 
-    for e in (jsonStr["data"]["pages"]["docs"]):
-        print(e["media"]["originalName"])
-        dowloadImg(e["media"]["path"], e["media"]["originalName"])
+    title = jsonStr["data"]["comic"]["title"]
 
-getImgs()
+    print(title)
+
+    getImgs(id, title)
+
+
+def getImgs(id, title):
+
+    page = 1
+
+    pageTotal = 10
+
+    while page < pageTotal:
+
+        url = 'https://picaapi.picacomic.com/comics/'+id+'/order/1/pages?page='+str(page)
+
+        user_agent = 'sora/2.2 (com.picacomic.sora; build:2.2; iOS 10.0.1) Alamofire/4.0.1'
+
+        headers = {
+            'User-Agent': user_agent,
+            'Host': 'picaapi.picacomic.com',
+            'Accept': 'application/vnd.picacomic.com.v1+json',
+            'Authorization':TOKEN,
+            'app-version': '2.0.1.3',
+            'api-key': '2587EFC6F859B4E3A1D8B6D33B272',
+            'app-platform': 'ios',
+            #    'Accept-Encoding': 'gzip;q=1.0, compress;q=0.5',
+            #    'Accept-Language': 'zh-Hans-CN;q=1.0',
+            'app-uuid': '8E4C6215-FF1E-4661-986C-226B841B8138',
+            'Connection': 'keep-alive'
+        }
+
+        full_url = urllib.request.Request(url, headers=headers)
+
+        response = urllib.request.urlopen(full_url)
+
+        the_page = response.read().decode("utf-8")
+
+        jsonStr = json.loads(the_page)
+
+        for e in (jsonStr["data"]["pages"]["docs"]):
+            print(e["media"]["originalName"])
+            dowloadImg(e["media"]["path"], e["media"]["originalName"], title)
+
+        pageTotal = jsonStr["data"]["pages"]["pages"]
+        page = page + 1
+
+login()
 
